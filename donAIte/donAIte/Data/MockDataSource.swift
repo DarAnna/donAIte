a876f4e19788eb0802b2595bcb4c7f014f3eacf2
Anna Darii <anna.darii@tum.de>
2021-10-16 00:23:47 +0200

    set up basic folder structure

---
+++ donAIte/donAIte/Data/MockDataSource.swift
@@ -0,0 +1,3 @@
+public class MockDataSource: DataSourceProtocol {
+    
+}
---
+++ donAIte/donAIte/Model/Organization.swift
@@ -0,0 +1,6 @@
+import Foundation
+public struct Organization {
+    public var name: String
+    public var link: URL
+    public var category: String
+}
